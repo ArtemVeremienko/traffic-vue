635e1da4ec66c35041a902bb34ad897c8f3a976d
Artem <temaspsxxx@gmail.com>
2020-10-20 16:31:37 +0300

    feat: add left menu component

---
+++ src/components/LeftSideMenu.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="left-side-menu mm-active" id="left-side-bar">
+    <div class="slimScrollDiv mm-show">
+      <div class="slimscroll-menu mm-active" id="left-side-menu-container">
+        <ul class="mt-3 metismenu side-nav mm-show" id="left-bar-menu">
+          <li class="side-nav-title side-nav-item">Список 1</li>
+          <li class="side-nav-item mm-active">
+            <a href="#" class="side-nav-link active"
+              ><i class="dripicons-web"></i> <span>Сайты</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-scale"></i> <span>Категории</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-scale"></i> <span>Страницы</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-scale"></i> <span>Запросы</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-scale"></i> <span>Выборки</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-scale"></i> <span>Исполнители</span></a
+            >
+          </li>
+
+          <li class="side-nav-title side-nav-item mt-2">Список 2</li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-document-edit"></i> <span>Трафик</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-document-edit"></i> <span>Рынок</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-blog"></i>
+              <span>Технический монитор</span></a
+            >
+          </li>
+          <li class="side-nav-item">
+            <a href="#" class="side-nav-link"
+              ><i class="dripicons-checklist"></i> <span>Отчёты</span></a
+            >
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftSideMenu",
+};
+</script>
+
+<style lang="scss">
+</style>
