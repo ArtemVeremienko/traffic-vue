f4c4f1ce59904a1595619839050d6829f599ded7
Artem <temaspsxxx@gmail.com>
2020-10-20 16:32:27 +0300

    feat: add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">2018 - 2020 © Traffic</div>
+        <div class="col-md-6">
+          <div class="text-md-right footer-links d-none d-md-block">
+            <a href="#" target="_blank">About</a>
+            <a href="#" target="_blank">Support</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>template
