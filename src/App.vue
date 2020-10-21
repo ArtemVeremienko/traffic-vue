3cb5afbe76025cba3f8fe1f375ce993b017792d2
Artem <temaspsxxx@gmail.com>
2020-10-21 19:37:53 +0300

    fix: show nav menu

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/auth">Authentification</router-link> |
+      <router-link to="/registration">Registration</router-link>
     </div>
     <router-view />
   </div>
@@ -11,4 +12,11 @@
 <style lang="scss">
 @import "assets/scss/app.scss";
 @import "assets/scss/icons.scss";
+
+#nav {
+  z-index: 9999;
+  position: absolute;
+  background: rgb(0 0 0 / 0.6);
+  padding: 0.5rem;
+}
 </style>
