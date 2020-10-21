55c87a4b519fe70005b1adde3a62126acfe22835
Artem <temaspsxxx@gmail.com>
2020-10-21 19:54:34 +0300

    fix: correct word

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/auth">Authentification</router-link> |
+      <router-link to="/auth">Authentication</router-link> |
       <router-link to="/registration">Registration</router-link>
     </div>
     <router-view />
