e89c044f33cd351789df5128bdbd9652d0fccdd5
Artem <temaspsxxx@gmail.com>
2020-10-22 13:33:29 +0300

    feat: add / route

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('login')) next('/home')
+      else next('/auth')
+    }
   },
   {
     path: '/auth',
@@ -19,7 +20,13 @@ const routes = [
     path: '/registration',
     name: 'Registration',
     component: () => import('../views/Registration.vue')
-  }
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+  },
+
 ]
 
 const router = new VueRouter({
