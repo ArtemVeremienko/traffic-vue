c0f5dfbdd24ca8957cd29b911d4514dad94b86d0
Artem <temaspsxxx@gmail.com>
2020-10-24 13:47:58 +0300

    refactor: remove unused values

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    authURL: 'https://seo-kit.ru/api/v1/auth',
-    regURL: 'https://seo-kit.ru/api/v1/user',
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
+    accessToken: '',
     user: {}
   },
   mutations: {
     setUser(state, payload) {
       state.user = payload
+    },
+    setToken(state, token) {
+      state.accessToken = token;
     }
   },
   actions: {
