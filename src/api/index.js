24d37e8f38af0764fc02fd20bea569ba24d2f768
Artem <temaspsxxx@gmail.com>
2020-10-24 13:38:28 +0300

    feat: add registration, authorization and cookie api

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const URL = 'https://seo-kit.ru/api/v1'
+const authURL = `${URL}/auth`
+const regURL = `${URL}/user`
+
+export const registration = async (data) => await axios.post(regURL, data)
+
+export const authorization = async (data) => await axios.post(authURL, data)
+
+export const setCookie = (name, value, options = {}) => {
+
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+export const setSecureCookie = (name, value) => {
+  setCookie(name, value, {
+    secure: true,
+    httpOnly: true,
+  })
+}
