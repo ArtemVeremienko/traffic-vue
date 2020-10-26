d7f1adf96dcd0c6478f61576415ab6bc2147bb9a
Artem <temaspsxxx@gmail.com>
2020-10-26 18:19:08 +0200

    feat: add mockup segment table

---
+++ src/components/TableSegments.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <table class="table table-striped table-centered mb-0">
+        <thead>
+          <tr>
+            <th>Тип трафика</th>
+            <th>Дата</th>
+            <th>Дата</th>
+            <th>Действия</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="idx in 5" :key="idx">
+            <td class="table-user">
+              <img
+                src="@/assets/images/users/avatar-2.jpg"
+                alt="table-user"
+                class="mr-2 rounded-circle"
+              />
+              Risa D. Pearson
+            </td>
+            <td>AC336 508 2157</td>
+            <td>July 24, 1950</td>
+            <td class="table-action">
+              <a href="#" class="action-icon">
+                <i class="mdi mdi-pencil"></i
+              ></a>
+              <a href="#" class="action-icon">
+                <i class="mdi mdi-delete"></i
+              ></a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  methods: {
+    random(num) {
+      return Math.floor(Math.random() * num);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
