241eb3ef13c42b4a0f539d1d5398e61605fdc456
Artem <temaspsxxx@gmail.com>
2020-10-26 17:45:08 +0200

    feat: add api for get traffic data

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,9 @@
 import axios from 'axios'
 
 const URL = 'https://seo-kit.ru/api/v1'
-const authURL = `${URL}/auth`
-const regURL = `${URL}/user`
 
 export const post = async ({ path, data }) => await axios.post(`${URL}/${path}`, data);
 
 export const getUser = async (id) => await axios.get(`${URL}/user/${id}`)
 
-export const registration = async (data) => await axios.post(regURL, data)
-
-export const authorization = async (data) => await axios.post(authURL, data)
+export const getTraffic = async () => await axios.get(`${URL}/SearchVisit?resource_id=1&start_date=2020-10-01&end_date=2020-10-20&group_by=type`)
