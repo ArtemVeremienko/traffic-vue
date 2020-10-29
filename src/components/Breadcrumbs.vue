246277fcbb52092701a4ca1f3e6d4bcf8849954b
Artem <temaspsxxx@gmail.com>
2020-10-29 21:27:24 +0200

    feat: add breadcrumbs component

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item active">
+        <router-link to="traffic">Траффик</router-link>
+      </li>
+      <li class="breadcrumb-item">
+        <router-link to="segments">Сегменты</router-link>
+      </li>
+      <li class="breadcrumb-item" aria-current="page">
+        <router-link to="">Конкуренты</router-link>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Breadcrumbs",
+};
+</script>
+
+<style>
+</style>
