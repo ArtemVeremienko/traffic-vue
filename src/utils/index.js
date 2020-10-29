503a8f01df46b6e82d02851c87da1984a66d94b8
Artem <temaspsxxx@gmail.com>
2020-10-29 21:15:51 +0200

    feat: add map rows for tables

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,3 +11,16 @@ export const transform = (data) => {
   }
   return table
 }
+
+export const rowsMap = {
+  total_brand: "Google",
+  total_brand_share: "Google Brand",
+  total_non_brand: "Google Non Brand",
+  total_non_brand_share: "Google Brand / Non Brand",
+  total_total: "All",
+  yandex_brand: "Yandex",
+  yandex_brand_share: "Yandex Brand",
+  yandex_non_brand: "Yandex Non Brand",
+  yandex_non_brand_share: "Yandex Brand / Yandex Non Brand",
+  yandex_total: "Yandex Brand + Yandex Non Brand",
+}
